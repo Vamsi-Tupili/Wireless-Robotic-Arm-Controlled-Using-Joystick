f3663f7feb12f562e5e633c49d576826cbc37c6d
Vamsi-Tupili <152056717+Vamsi-Tupili@users.noreply.github.com>
2025-02-02 22:16:27 +0530

    Add files via upload

---
+++ Robotic_arm_Tx_bluetooth.ino
@@ -0,0 +1,121 @@
+
+#define sw 5
+#define screenWidth 900
+#define screenHeight 900
+int value = 0;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+void loop()
+{
+  int X = analogRead(A0);
+  int Y = analogRead(A1);
+  int X1 = analogRead(A2);
+  int Y1 = analogRead(A3);
+  int x = map(X, 0, 1023,0,screenWidth);
+  int y = map(Y, 0, 1023,0,screenHeight); 
+  int x1 = map(X1, 0, 1023,0,screenWidth);
+  int y1 = map(Y1, 0, 1023,0,screenHeight);
+  int sStat = digitalRead(sw);
+    if(x >= 550)
+  {
+    Serial.println("F");
+    Serial.println(x);
+    Serial.println(y);
+    Serial.println("Forward");
+    delay(200);
+  }
+  else if(x <= 250)
+  {
+    Serial.println("B");
+    Serial.println(x);
+    Serial.println(y);
+    Serial.println("Backward");
+    delay(200);
+  }
+  else if(y <= 250)
+  {
+    Serial.println("L");
+    Serial.println(x);
+    Serial.println(y);
+    Serial.println("Left");
+    delay(200);
+  }
+  else if(y >= 650)
+  {
+    Serial.println("R");
+    Serial.println(x);
+    Serial.println(y);
+    Serial.println("Right");
+    delay(200);
+  }
+  if(x1 >= 550)
+  {
+    Serial.println("M");
+    Serial.println(x1);
+    Serial.println(y1);
+    Serial.println("Forward");
+    delay(200);
+  }
+  else if(x1 <= 250)
+  {
+    Serial.println("N");
+    Serial.println(x1);
+    Serial.println(y1);
+    Serial.println("Backward");
+    delay(200);
+  }
+  else if(y1 <= 250)
+  {
+    Serial.println("O");
+    Serial.println(x1);
+    Serial.println(y1);
+    Serial.println("Left");
+    delay(200);
+  }
+  else if(y1 >= 650)
+  {
+    Serial.println("P");
+    Serial.println(x1);
+    Serial.println(y1);
+    Serial.println("Right");
+    delay(200);
+  }
+  /*else if(x >= 700 && y <= 150)
+  {
+    Serial.println("A");
+    delay(500);
+    Serial.println(x);
+    Serial.println(y);
+    Serial.println("Spin Left");
+    delay(200);
+  }
+  else if(x >= 700 && y >= 700)
+  {
+    Serial.println("C");
+    Serial.println(x);
+    Serial.println(y);
+    Serial.println("Spin Right");
+    delay(200);
+  }
+  else if(x == 150 && y == 150)
+  {
+    Serial.println("D");
+    Serial.println(x);
+    Serial.println(y);
+    Serial.println("Spin Right");
+    delay(200);
+  }
+  else if(x <= 100 && y >= 700)
+  {
+    Serial.println("E");
+    Serial.println(x);
+    Serial.println(y);
+    Serial.println("Spin left");
+    delay(200);
+  }*/
+  delay(200);
+}
+
+
---
+++ Robotic_arm_recv_bluetooth.ino
@@ -0,0 +1,111 @@
+
+#include<Servo.h>
+int pos = 90;
+int pos1 = 90;
+int pos2 = 90;
+int pos3 = 90;
+
+Servo s1;
+Servo s2;
+Servo s3;
+Servo s4;
+
+void setup() 
+{
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  s1.attach(3);          //Servo motor attached to pin 3
+  s1.write(pos);
+  s2.attach(5);
+  s2.write(pos1);
+  s3.attach(10);
+  s3.write(pos2);
+  s4.attach(11);
+  s4.write(pos3);
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  if(Serial.available())
+  {
+    char val = Serial.read();
+    if(val == 'F')
+    {
+      Serial.println(val);
+      if(pos <= 180)
+      {
+        pos = pos + 20;
+        s1.write(pos);
+        Serial.println("Servo reached maximum");
+       }
+    }
+    else if(val == 'B')
+    {
+      Serial.println(val);
+      if(pos >= 0)
+  {
+   pos = pos - 20;
+   s1.write(pos);
+  }
+    }
+    else if(val == 'L')
+    {
+     if(pos1 <= 180)
+  {
+  pos1 = pos1 + 20;
+  s2.write(pos1);
+  }
+  digitalWrite(3,HIGH);
+    }
+    else if(val == 'R')
+    {
+      if(pos1 >= 0)
+  {
+   pos1 = pos1 - 20;
+   s2.write(pos1);
+  }
+    }
+    else if(val == 'M')
+    {
+      Serial.println(val);
+      if(pos2 <= 180)
+  {
+  pos2 = pos2 + 20;
+  s3.write(pos2);
+  }
+    }
+    else if(val == 'N')
+    {
+      Serial.println(val);
+      if(pos2 >= 0)
+  {
+   pos2 = pos2 - 20;
+   s3.write(pos2);
+  }
+    }
+    else if(val == 'O')
+    {
+     if(pos3 <= 180)
+  {
+  pos3 = pos3 + 20;
+  s4.write(pos3);
+  }
+    }
+    else if(val == 'P')
+    {
+      if(pos3 >= 0)
+  {
+   pos3 = pos3 - 20;
+   s4.write(pos3);
+  }
+    }
+    else
+    {
+      Serial.print("stop");
+    }
+  }
+}
+
+
+
